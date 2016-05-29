dca308155ed55a373d4aff80e0a83472f00c2e7c
rjbultitude <richard.bultitude@gmail.com>
2016-05-29 16:38:06 +0100

    add server side login script

---
+++ sc-login.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once './vendor/autoload.php';
+include_once './client-data.php';
+
+use Njasm\Soundcloud\SoundcloudFacade;
+
+$facade = new SoundcloudFacade($clientID, $clientSecret);
+$facade->userCredentials($username, $password); // on success, access_token is set by default for next requests.
+$response = $facade->get('/me')->request();
+// raw/string body response
+echo $response->bodyRaw();
+// as object
+echo $response->bodyObject()->id;
+// as array
+$array = $response->bodyArray();
+print_r($array);
